232934c035ad735e899e9e07004da7820cc26863
RondBallon <clement.ferrandery@gmail.com>
2024-09-04 10:12:42 +0200

    Fetch + Popup
    Fetch+Popup

---
+++ background.js
@@ -0,0 +1,7 @@
+// Exemple simple de background.js
+// Ce script peut rester vide si vous n'avez pas de fonctionnalités spécifiques à gérer en arrière-plan
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installée");
+});
+
